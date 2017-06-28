7d0ad05c1fd2845179b23e34a2d766e7474c588c
weimingchang1 <1642707703@qq.com>
2017-06-28 23:11:08 +0800

    first commit

---
+++ app/src/main/java/com/example/wmc/jkbd/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.wmc.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by wmc on 2017/6/28.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mCountDownTimer.start();
+    }
+
+    CountDownTimer mCountDownTimer  = new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    };
+}
