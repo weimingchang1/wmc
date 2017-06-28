94eb4a8fc3ddc992215f9da2f0ccf7ad84e42124
weimingchang1 <1642707703@qq.com>
2017-06-28 23:56:22 +0800

    添加试题Question实体类

---
+++ app/src/main/java/com/example/wmc/jkbd/bean/Question.java
@@ -0,0 +1,101 @@
+package com.example.wmc.jkbd.bean;
+
+/**
+ * Created by wmc on 2017/6/28.
+ */
+
+public class Question {
+    /**
+     * id : 8
+     * question : 这个标志是何含义？
+     * answer : 2
+     * item1 : 车道方向预告
+     * item2 : 交叉路口预告
+     * item3 : 分道信息预告
+     * item4 : 分岔处预告
+     * explains : 此为交叉路口预告，记住吧。
+     * url : http://images.juheapi.com/jztk/c1c2subject1/8.jpg
+     */
+
+    private int id;
+    private String question;
+    private String answer;
+    private String item1;
+    private String item2;
+    private String item3;
+    private String item4;
+    private String explains;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getExplains() {
+        return explains;
+    }
+
+    public void setExplains(String explains) {
+        this.explains = explains;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
