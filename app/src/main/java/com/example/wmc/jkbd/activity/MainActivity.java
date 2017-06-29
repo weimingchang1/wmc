e108c7630a37827eca13fbf333065d1329083c5d
weimingchang1 <1642707703@qq.com>
2017-06-30 00:06:53 +0800

    通过OKhttp下载考卷数据

--- app/src/main/java/com/example/wmc/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/wmc/jkbd/activity/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.SharedElementCallback;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.wmc.jkbd.R;
+import com.example.wmc.jkbd.bean.Examination;
+import com.example.wmc.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        OkHttpUtils<Examination> utils=new OkHttpUtils<>(getApplicationContext());
+        String url="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(url)
+                .targetClass(Examination.class)
+                .execute(new OkHttpUtils.OnCompleteListener<Examination>() {
+                    @Override
+                    public void onSuccess(Examination result) {
+                        Log.e("main","result="+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
 
