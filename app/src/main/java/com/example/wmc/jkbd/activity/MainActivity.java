0f9f9dd4143ab3b1e04c5d6125da7a9fe82ece84
weimingchang1 <1642707703@qq.com>
2017-06-29 22:32:02 +0800

    点击随机事件进入考试界面

--- app/src/main/java/com/example/wmc/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/wmc/jkbd/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.wmc.jkbd.activity;
 
+import android.app.SharedElementCallback;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
