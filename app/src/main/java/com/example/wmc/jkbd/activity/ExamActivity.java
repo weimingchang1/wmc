8aa7ed8ec160cfebe94582d2268fad76f1b7d5fa
weimingchang1 <1642707703@qq.com>
2017-07-06 22:32:45 +0800

    保存答案后将选项锁定

--- app/src/main/java/com/example/wmc/jkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/wmc/jkbd/activity/ExamActivity.java
@@ -320,6 +320,7 @@ public class ExamActivity extends AppCompatActivity {
         for (int i = 0; i < cbs.length; i++) {
             if (cbs[i].isChecked()) {
                 biz.getExam().setUserAnswer(String.valueOf(i + 1));
+                setOptions(true);
                 mAdapter.notifyDataSetChanged();
                 return;
             }
